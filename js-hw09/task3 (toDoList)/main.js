101f958daaf76df278a134c19948321a937a5b9e
“lizazp92” <iamfromsirius@gmail.com>
2024-05-05 00:30:42 +0400

    initial setup for todolist

---
+++ js-hw09/task3 (toDoList)/main.js
@@ -0,0 +1,4 @@
+/* Create a simple to-do list page using jQuery. 
+Allow users to add and remove tasks. 
+Each task should have a checkbox to mark it as complete. 
+Change style of row when task is marked as complete */
